625fd623554354d631a7c05d11e77e0efbabcd90
shafilahaf <shafilah.f@students.amikom.ac.id>
2021-02-05 19:04:54 +0700

    attendee section

--- __manifest__.py
+++ __manifest__.py
@@ -9,7 +9,8 @@ this is my academic information system module
 """,
 	"data" : ["menu.xml",
 		"course.xml",
-		"session.xml"
+		"session.xml",
+		"attendee.xml"
 		],
 	"installable": True,
 	"auto_install" : False,
