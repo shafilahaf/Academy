538a11fd207d45a59399147dd38d6bd075066c37
shafilahaf <shafilah.f@students.amikom.ac.id>
2021-02-24 12:20:52 +0700

    Kanban View

--- session.py
+++ session.py
@@ -10,13 +10,15 @@ class session(models.Model):
     instructor_id = fields.Many2one('res.partner', 'Instructor')
     name = fields.Char('Name', size=100, required=True)
     start_date = fields.Date(default=fields.Date.today)
-    duration = fields.Integer('Duration')
+    duration = fields.Integer('Duration', help="Duration in days")
     seats = fields.Integer('Number of Seats')
     active = fields.Boolean(default=True)
+    color = fields.Integer()
     attendee_ids = fields.One2many('academic.attendee', 'session_id', 'Attendess', ondelete="cascade")
     taken_seats = fields.Float(string="Taken seats", compute='_taken_seats', store=False)
     end_date = fields.Date(string="End Date", store=True, compute='_get_end_date', inverse='_set_end_date')
     hours = fields.Float(string="Duration in Hours", compute='_get_hours', inverse='_set_hours')
+    attendees_count =  fields.Integer(string="Attendees Count", compute='_get_attendees_count', store=True)
 
     @api.depends('seats', 'attendee_ids')
     def _taken_seats(self):
@@ -55,6 +57,11 @@ class session(models.Model):
     def _set_hours(self):
         for r in self:
             r.duration = r.hours / 24
+
+    @api.depends('attendee_ids')
+    def _get_attendees_count(self):
+        for r in self:
+            r.attendees_count = len(r.attendee_ids)
 
     #The “onchange” mechanism provides a way for the client interface to 
     # update a form whenever the user has filled in a value in a field, 
